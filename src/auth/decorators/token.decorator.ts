661e0a3242446216efe1347b5a26253ada19dff1
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-12-13 02:13:20 -0500

    add auth microservice

---
+++ src/auth/decorators/token.decorator.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const Token = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    if (!request.token) {
+      throw new InternalServerErrorException(
+        'Token not found in request (AuthGuard called?)',
+      );
+    }
+
+    return request.token;
+  },
+);
---
+++ src/auth/decorators/user.decorator.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    if (!request.user) {
+      throw new InternalServerErrorException(
+        'User not found in request (AuthGuard called?)',
+      );
+    }
+
+    return request.user;
+  },
+);
